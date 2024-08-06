493b59fabac6f59089e94c583c0f9a72c873c974
Joan <joan.almeida90@gmail.com>
2024-08-06 14:26:09 -0300

    Print request body

--- app/api/[...all]/route.ts
+++ app/api/[...all]/route.ts
@@ -3,5 +3,7 @@ import { type NextRequest } from "next/server";
 export async function POST(request: NextRequest) {
   console.log(`Got a request for ${request.url}`);
   console.log(`Search params: ${request.nextUrl.searchParams}`);
+  const body = await request.json();
+  console.log(body);
   return new Response("Ok!", { status: 200 });
 }
